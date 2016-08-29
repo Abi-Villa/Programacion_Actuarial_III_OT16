2718df1ad69460a4be0c5de20648bdc1e2d2a9d7
Abigail Villa <abigail.villa102@gmail.com>
2016-08-29 09:48:39 -0500

    Agregar archivo de códigos de clase
    Se agregó el script en el que he estado trabajando toda la clase y que
    muestra los primeros comandos de R para trabajar

---
+++ Avance de clase.R
@@ -0,0 +1,30 @@
+# Matrices
+m <- matrix(nrow = 2,ncol = 3)
+m
+dim(m)
+attributes(m)
+# Cambio de dimensiones de 2x3 a 3x2
+dim(m) <- c(3,2)
+m
+# Crear una matriz con datos
+m <- matrix(1:6,3,2)
+m
+
+m <- matrix(1:6,3,3,TRUE)
+m
+class(m)
+str(m)
+
+dim(m) <- c(2,5) #Esto va a producir error
+
+x <- c(1,2,3)
+y <- c("a","b","c")
+z <- c(x,y)
+z
+
+m1 <- rbind(m,y)
+m1
+
+m2 <- cbind(m,y)
+m2
+
