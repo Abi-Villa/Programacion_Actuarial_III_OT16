e60bbf2679767ae5e3310b6b4e1279ae59c46b2a
Abigail Villa <abigail.villa102@gmail.com>
2016-08-31 09:43:32 -0500

    Se agregó una primer base de datos
    Se descargó de Yahoo Finance la base de datos de precios de S&P y se
    utilizó para ejemplificar el uso de read.table y read.csv

--- Avance de clase.R
+++ Avance de clase.R
@@ -28,3 +28,59 @@ m1
 m2 <- cbind(m,y)
 m2
 
+#Factores
+
+x <- factor(c("si","no","si","si","no","si","no"))
+x
+table(x)
+unclass(x)
+x <- factor(c("si","no","si","si","no","si","no"),levels = c("si","no"))
+
+x <- factor(c("azul","azul","rojo","azul","amarillo","verde","azul"))
+x
+table(x)
+
+#Valores Faltantes
+x <- c(1,2,NaN,10,3)
+is.na(x)
+is.nan(x)
+
+#Data Frames
+x <- data.frame(Erick = 1:4, Lori = c(T,T,F,F))
+row.names(x) <- c("Primero","Segundo","Tercero","cuarto")
+x
+nrow(x)
+ncol(x)
+attributes(x)
+names(x) <- c("Yarely","Karen")
+x
+
+#Los nombres no son exclusivos de los data frame
+x <- 1:3
+names(x) #NULL
+names(x) <- c("Hugo","Paco","Luis")
+x
+
+x <- list(a=1:10,b=100:91,c=51:60)
+x
+names(x) <-c("Seq1","Seq2","Seq3")
+x
+
+m <- matrix(1:4,2,2)
+m
+attributes(m)
+dimnames(m) <- list(c("fil1","fil2"),c("col1","col2"))
+m
+
+m <- matrix(NA,4,6)
+m
+dimnames(m) <- list(c("1","2","3","4"),c("A","B","C","D","E","F"))
+m
+
+#Lectura de datos
+getwd()
+setwd("~/GitHub/Programacion_Actuarial_III_OT16")
+data <- read.csv("table.csv")
+data <- read.table("table.csv",T,",")
+data
+
